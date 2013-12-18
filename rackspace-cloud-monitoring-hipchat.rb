63d951e3f1d49e0dfd1fdeffeafbc13627e566a5
Omar Qureshi <omar@buddyapp.org>
2013-12-18 17:05:37 +0000

    Reverse logic (oops), use symbols over strings

--- rackspace-cloud-monitoring-hipchat.rb
+++ rackspace-cloud-monitoring-hipchat.rb
@@ -12,14 +12,14 @@ class Object
 end
 
 $config = YAML::load_file(File.join(File.dirname(File.expand_path(__FILE__)), 'config.yml'))
-if $config['path_to_hipchat_credentials'].present?
-  external_credentials = YAML::load_file($config['path_to_hipchat_credentials'])
-  $config['hipchat_api'] = external_credentials['hipchat_api']
-  $config['hipchat_room'] = external_credentials['hipchat_room']
+if $config[:path_to_hipchat_credentials].present?
+  external_credentials = YAML::load_file($config[:path_to_hipchat_credentials])
+  $config[:hipchat_api] = external_credentials[:hipchat_api]
+  $config[:hipchat_room] = external_credentials[:hipchat_room]
 end
 
-if !($config['hipchat_api'].present? && $config['hipchat_room'].present?)
-  $room = HipChat::Client.new($config['hipchat_api'])[$config['hipchat_room']]
+if $config[:hipchat_api].present? && $config[:hipchat_room].present?
+  $room = HipChat::Client.new($config[:hipchat_api])[$config[:hipchat_room]]
 else
   raise "No Hipchat credentials are present"
 end
